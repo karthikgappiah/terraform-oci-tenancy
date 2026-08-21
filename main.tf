e92704059eaa7c500ab0a387305274f41e8eef2d
Karthik G. Appiah <122487930+karthikgappiah@users.noreply.github.com>
2026-08-20 20:49:36 -0700

    feat: create compartment 'free-compartment'

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
 data "oci_identity_tenancy" "this" {
   tenancy_id = var.tenancy_ocid
 }
+
+resource "oci_identity_compartment" "free" {
+  compartment_id = var.tenancy_ocid
+  name           = "free"
+  description    = "Compartment for OCI resources that are always free."
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,8 @@
+output "free_compartment_id" {
+  description = "OCID of the free compartment"
+  value       = oci_identity_compartment.free.id
+}
+
 output "tenancy_name" {
   description = "Name of the OCI tenancy"
   value       = data.oci_identity_tenancy.this.name
